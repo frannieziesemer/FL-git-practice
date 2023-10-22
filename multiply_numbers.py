1fe24677451a5c1cc05011dec4d425a389a9db04
Frannie Ziesemer <52860762+frannieziesemer@users.noreply.github.com>
2023-10-22 19:16:18 +0200

    Create multiply_numbers.py

---
+++ multiply_numbers.py
@@ -0,0 +1,6 @@
+def multiply_numbers(a, b):
+    """
+    Multiplies two numbers and returns the result.
+    """
+    # Intentional error: Using addition instead of multiplication
+    return a + b
