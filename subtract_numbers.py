3a516e9c244086c53975e986122a2d20ce349e8b
Frannie Ziesemer <52860762+frannieziesemer@users.noreply.github.com>
2023-10-22 19:15:46 +0200

    Create subtract_numbers.py

---
+++ subtract_numbers.py
@@ -0,0 +1,6 @@
+def subtract_numbers(a, b):
+    """
+    Subtracts the second number from the first and returns the result.
+    """
+    # Intentional error: Swapped the order of subtraction
+    return b - a
