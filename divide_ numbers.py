3851ebf5c171b86423dffc3a435a623f1503602c
Frannie Ziesemer <52860762+frannieziesemer@users.noreply.github.com>
2023-10-23 17:19:50 +0200

    Update divide_ numbers.py

--- divide_ numbers.py
+++ divide_ numbers.py
@@ -1,14 +1,7 @@
 
-def divide_numbers(a, b):
+def divide_numbers(a, b, c):
     """
     Divides the first number by the second and returns the result.
     """
     # Intentional error: No check for division by zero
-    return a / b
-
-def merge_conflict_function(x, y):
-    """
-    Function with two errors on one line.
-    """
-    # Intentional errors: Reversed the order of subtraction and used + instead of -
-    return x + y
+    return a * b
